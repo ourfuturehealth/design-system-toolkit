4d0429db9211039ef0befec2aa9925c04e2449ee
Simon Plenderleith <108673524+simonplend-yld@users.noreply.github.com>
2022-09-26 15:56:24 +0100

    Clean up code in the documentation site (#23)
    * Fix generation of site isolated design examples
    
    We've added support for design examples that use the `fullpage` and
    `blankpage` design example layouts.
    
    For every design example under `site/views/design-system/` we generate
    a page for each design example layout (default, fullpage, blankpage)
    e.g. for `site/views/design-system/components/buttons/reverse/index.njk`:
    
    - /design-example/components/buttons/reverse/layout-default/
    - /design-example/components/buttons/reverse/layout-fullpage/
    - /design-example/components/buttons/reverse/layout-blankpage/
    
    * Remove site cookie consent JavaScript
    
    We're not using this.
    
    * Remove site search and autocomplete JavaScript
    
    We're not using this.
    
    Former-commit-id: f123173939a6855fbddf2d86e9fd4e1e9e501b3f [formerly e1648d9f2592eb15d3390ac288deca2a21c65828]
    Former-commit-id: 563f1bd9b265e9c51986707e87d079edf3490132

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-new */
 // Design system toolkit components
 import MenuToggle from '../../packages/components/header/menuToggle';
-import SearchToggle from '../../packages/components/header/searchToggle';
 import SkipLink from '../../packages/components/skip-link/skip-link';
 import Details from '../../packages/components/details/details';
 import Checkboxes from '../../packages/components/checkboxes/checkboxes';
 import Radios from '../../packages/components/radios/radios';
 import Card from '../../packages/components/card/card';
-
-// import AutoComplete from './autocomplete/autoCompleteConfig';
 
 // Toolkit polyfills
 import '../../packages/polyfills';
@@ -16,31 +13,8 @@ import '../../packages/polyfills';
 // Documentation site example components
 import DesignExample from './design-example';
 
-// TODO: Remove this?
-// import {
-//   inputValue,
-//   onConfirm,
-//   source,
-//   suggestion,
-// } from './search';
-
 // Initialise toolkit components
-
-// TODO: Remove this?
-// AutoComplete({
-//   containerId: 'autocomplete-container',
-//   formId: 'search',
-//   inputId: 'search-field',
-//   onConfirm,
-//   source,
-//   templates: {
-//     inputValue,
-//     suggestion,
-//   },
-// });
-
 MenuToggle();
-SearchToggle();
 Details();
 SkipLink();
 Checkboxes();
