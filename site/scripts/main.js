e09c5666d24a19a27fb65e646a2761e79279f9f6
Richard da Silva <richard.dasilva@ourfuturehealth.org.uk>
2025-01-09 18:23:14 +0000

    Autocomplete

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -6,6 +6,7 @@ import Details from '../../packages/components/details/details';
 import Checkboxes from '../../packages/components/checkboxes/checkboxes';
 import Radios from '../../packages/components/radios/radios';
 import Card from '../../packages/components/card/card';
+import AutoComplete from '../../packages/components/auto-complete/auto-complete';
 
 // Toolkit polyfills
 import '../../packages/polyfills';
@@ -20,6 +21,7 @@ SkipLink();
 Checkboxes();
 Radios();
 Card();
+AutoComplete();
 
 // Initialise documentation site components
 
