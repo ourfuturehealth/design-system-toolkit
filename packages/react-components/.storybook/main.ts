46fe64fad9225e16f4f3c13110a812e2ba7e069c
Diogo Costa <diogo.costa@ourfuturehealth.org.uk>
2026-03-10 12:25:21 +0000

    style: lint issues

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -1,12 +1,12 @@
-import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [getAbsolutePath("@storybook/addon-docs"), getAbsolutePath("@storybook/addon-a11y")],
+  addons: [getAbsolutePath('@storybook/addon-docs'), getAbsolutePath('@storybook/addon-a11y')],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
   typescript: {
