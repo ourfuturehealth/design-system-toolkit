d59d544ed1ba3d95f5deeaddd2062628940a7607
Diogo Costa <diogo.costa@ourfuturehealth.org.uk>
2026-04-09 11:23:02 +0100

    chore(icons): preserve stepper number order
    Keep the Stepper gallery rows in 0 through 9 order while leaving the other icon categories alphabetical.

--- packages/site/views/_data/materialIcons.js
+++ packages/site/views/_data/materialIcons.js
@@ -21,7 +21,26 @@ const iconsByCategory = categoryOrder
   .map((category) => {
     const categoryIcons = icons.filter((icon) => icon.category === category);
 
-    categoryIcons.sort((a, b) => a.name.localeCompare(b.name));
+    if (category === 'Stepper') {
+      const stepperOrder = [
+        'ListZero',
+        'ListOne',
+        'ListTwo',
+        'ListThree',
+        'ListFour',
+        'ListFive',
+        'ListSix',
+        'ListSeven',
+        'ListEight',
+        'ListNine',
+      ];
+
+      categoryIcons.sort((a, b) => {
+        return stepperOrder.indexOf(a.name) - stepperOrder.indexOf(b.name);
+      });
+    } else {
+      categoryIcons.sort((a, b) => a.name.localeCompare(b.name));
+    }
 
     return {
       category,
